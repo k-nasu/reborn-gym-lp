c0454b3749fb3c37943e566d8f8e68426dadd209
kenta.nasu <k.nasu1996@gmail.com>
2022-11-19 20:19:32 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Layout from "../components/Layout";
-import KeyVisual from "../components/KeyVisual";
-// import Campaign from "../components/Campaign";
+// import KeyVisual from "../components/KeyVisual";
+import Campaign from "../components/Campaign";
 import ConceptTop from "../components/ConceptTop"
 import ConceptContent from "../components/ConceptContent"
 import FlowTop from "../components/FlowTop"
@@ -14,8 +14,8 @@ import Faq from "../components/Faq";
 
 export default function Home() {
   return <Layout>
-    <KeyVisual />
-    {/* <Campaign /> */}
+    {/* <KeyVisual /> */}
+    <Campaign />
     <ConceptTop />
     <ConceptContent />
     <FlowTop />
@@ -23,7 +23,7 @@ export default function Home() {
     <Trainer />
     <MenuForReborn />
     <MenuForStartup />
-    {/* <Campaign /> */}
+    <Campaign />
     <Competitor />
     <Review />
     <Faq />
