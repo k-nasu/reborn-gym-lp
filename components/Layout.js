13f49a26da7d4fb478a1c18ab1154d0dca8f59a8
kenta.nasu <k.nasu1996@gmail.com>
2022-11-19 20:08:14 +0900

    change how to apply container css

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,21 @@
+import { useLayoutEffect } from "react";
 import Head from "next/head";
 import Footer from "./Footer";
 import styles from "./layout.module.css";
 
 function Layout({ children }) {
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0)
+}, []);
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>re.bornGYM</title>
         <meta name="description" content="美容整体サロンと併設している業界唯一のパーソナルトレーニングジム" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>{children}</main>
+      <main className={styles.container}>{children}</main>
       <Footer />
     </div>
   );
