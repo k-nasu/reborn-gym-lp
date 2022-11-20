7b95e86545ea0807d805c31c17798daf343ca1b9
kenta.nasu <k.nasu1996@gmail.com>
2022-11-20 21:10:36 +0900

    change position of container css

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,21 @@
-import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 import Head from "next/head";
 import Footer from "./Footer";
 import styles from "./layout.module.css";
 
 function Layout({ children }) {
-  useLayoutEffect(() => {
+  useEffect(() => {
     window.scrollTo(0, 0)
 }, []);
 
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>re.bornGYM</title>
         <meta name="description" content="美容整体サロンと併設している業界唯一のパーソナルトレーニングジム" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.container}>{children}</main>
+      <main>{children}</main>
       <Footer />
     </div>
   );
