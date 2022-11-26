36aa15ea40420fce45168732b07e284252c4a34c
kenta.nasu <k.nasu1996@gmail.com>
2022-11-26 18:50:56 +0900

    modify google tag configuration

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,10 @@
 import Head from "next/head";
 import { DefaultSeo } from 'next-seo';
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return <>
     <Head>
-      {/* <!-- Google Tag Manager --> */}
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-MLHX4NP');</script>
-      {/* <!-- End Google Tag Manager --> */}
       <title>re.born GYM</title>
       <meta
         name="viewport"
@@ -18,6 +12,17 @@ function MyApp({ Component, pageProps }) {
       />
       <link rel="icon" href="/favicon.ico" />
     </Head>
+    {/* Google Tag Manager */}
+    <Script id="google-analytics">
+      {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MLHX4NP');
+      `}
+    </Script>
+    {/* End Google Tag Manager */}
     <DefaultSeo
         defaultTitle="恵比寿【美容整体×パーソナルトレーニング】|re.born GYM"
         description="初心者・女性の方の満足度No.1 美容整体サロンと併設している業界唯一のパーソナルトレーニングジム。恵比寿駅から徒歩8分、代官山駅から徒歩5分のロケーション。re.born GYMでは、痛みの軽減、美しい体づくりをサポートしていきます。"
--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,13 @@ export default function Home() {
   
   return <>
     {/* <!-- Google Tag Manager (noscript) --> */}
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MLHX4NP"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <noscript>
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MLHX4NP"
+        height="0"
+        width="0"
+        style="display:none;visibility:hidden"
+      />
+    </noscript>
     {/* <!-- End Google Tag Manager (noscript) --> */}
     <div className={styles.container}>
       <KeyVisual />
