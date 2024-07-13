68120ac7f6db35715861821a6615c52e95fb38d1
kenta.nasu <46446368+k-nasu@users.noreply.github.com>
2024-07-13 16:35:03 +0900

    change trainer pic from kyota to ai

--- components/Trainer.js
+++ components/Trainer.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Trainer = () => {
   return(
     <Image
-      src="/images/trainer.jpg"
+      src="/images/trainer.png"
       alt="トレーナーの画像"
       width={768}
       height={745}
